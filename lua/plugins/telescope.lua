70af025cc9a44de5aafab3e685a2a8bbe7e349c3
sebastian9991 <sebastian.sabry1@gmail.com>
2025-07-25 00:55:43 -0400

    update: adding gitfiles and word search on telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,9 +7,16 @@ return {
 			--Telescope
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            --Find only git files
+            vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+			--Project word search
+			vim.keymap.set("n", "<leader>pws", function()
+				local word = vim.fn.expand("<cword>")
+				builtin.grep_string({ search = word })
+			end)
 		end,
 	},
 	{
